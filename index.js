1c13dacd53435be67830b8cf4f088f439d686875
“Irfan <irfanzafar.dev@gmail.com>
2022-12-03 18:58:37 +0700

    [INIT] first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+const connect = () => {
+  mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to database!')
+  }).catch(err => {
+    throw err
+  })
+}
+
+app.listen(8080, () => {
+  connect()
+  console.log("connected to server!")
+})
